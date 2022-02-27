64fd1705c60c773d05f465faeaba938e16c228da
prem9141 <premkumar9141@gmail.com>
2022-02-26 20:45:34 -0500

    Added code to create all the five tables and insert records into them. Also updated the fetch record module to return dictionary

--- database.py
+++ database.py
@@ -1,12 +1,60 @@
 import mysql.connector as mysql
 from mysql.connector import errorcode
 
+DB_NAME = 'cs634_data_mining_project'
+
+config = {
+    'user': 'root',
+    'password': 'Maharaj3n$$$567',
+    'host': '127.0.0.1',
+    'raise_on_warnings': True
+}
+
+TABLES = dict()
+
+TABLES['database1'] = (
+        "CREATE TABLE `database1` ("
+        "  `trans_id` int NOT NULL AUTO_INCREMENT,"
+        "  `items` varchar(255) NOT NULL,"
+        "  PRIMARY KEY (`trans_id`)"
+        ") ENGINE=InnoDB")
+
+TABLES['database2'] = (
+        "CREATE TABLE `database2` ("
+        "  `trans_id` int NOT NULL AUTO_INCREMENT,"
+        "  `items` varchar(255) NOT NULL,"
+        "  PRIMARY KEY (`trans_id`)"
+        ") ENGINE=InnoDB")
+
+TABLES['database3'] = (
+        "CREATE TABLE `database3` ("
+        "  `trans_id` int NOT NULL AUTO_INCREMENT,"
+        "  `items` varchar(255) NOT NULL,"
+        "  PRIMARY KEY (`trans_id`)"
+        ") ENGINE=InnoDB")
+
+TABLES['database4'] = (
+        "CREATE TABLE `database4` ("
+        "  `trans_id` int NOT NULL AUTO_INCREMENT,"
+        "  `items` varchar(255) NOT NULL,"
+        "  PRIMARY KEY (`trans_id`)"
+        ") ENGINE=InnoDB")
+
+TABLES['database5'] = (
+        "CREATE TABLE `database5` ("
+        "  `trans_id` int NOT NULL AUTO_INCREMENT,"
+        "  `items` varchar(255) NOT NULL,"
+        "  PRIMARY KEY (`trans_id`)"
+        ") ENGINE=InnoDB")
+
 
 class Database:
-    def __init__(self, connect_params):
+    def __init__(self):
         try:
-            self.cnx = mysql.connect(**connect_params)
+            self.cnx = mysql.connect(**config)
             self.cursor = self.cnx.cursor()
+            self.dbname = DB_NAME
+            self.tables = None
         except mysql.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Something is wrong with your user name or password")
@@ -15,76 +63,70 @@ class Database:
             else:
                 print(err)
 
-    def create_database(self, db_name):
+    def create_database(self):
         try:
-            self.cursor.execute(f"CREATE DATABASE {db_name} DEFAULT CHARACTER SET 'utf8mb4'")
+            self.cursor.execute(f"CREATE DATABASE {self.dbname} DEFAULT CHARACTER SET 'utf8mb4'")
         except mysql.Error as err:
-            print(f"Failed creating database {db_name}: {err}")
+            print(f"Failed creating database {self.dbname}: {err}")
             exit(1)
 
-    def connect_database(self, db_name):
+    def connect_database(self):
         try:
-            self.cursor.execute(f'USE {db_name}')
+            self.cursor.execute(f'USE {self.dbname}')
         except mysql.Error as err:
-            print(f'{db_name} does not exists')
+            print(f'{self.dbname} does not exists')
             if err.errno == errorcode.ER_BAD_DB_ERROR:
-                print(f'Creating database {db_name}')
-                self.create_database(db_name)
-                self.cnx.dataset = db_name
+                print(f'Creating database {self.dbname}')
+                self.create_database(self.dbname)
+                self.cnx.dataset = self.dbname
             else:
                 print(err)
                 exit(1)
 
     def create_table(self, tables):
+        self.tables = tables;
         try:
-            for table in tables:
-                self.cursor.execute(tables[table])
+            for table in self.tables.keys():
+                self.cursor.execute(self.tables[table])
         except mysql.Error as err:
             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
                 print("already exists.")
             else:
                 print(err.msg)
 
+    def fetch_file_data(self, fname):
+        fname = "./" + fname + ".txt"
+        records = list()
+        with open(fname, 'r') as file:
+            for line in file.readlines():
+                line = line.strip()
+                trans_id, items = line.split(' ', 1)
+                records.append((int(trans_id), items))
+        return records
+
     def insert_records(self):
         try:
-            add_insert = "INSERT INTO DATASET_1 (TRANS_ID, ITEMS) VALUES (%s, %s)"
-            add_values = [(1, "Water, Cocoa, Cider"),
-              (2, "Cocoa, Water, Juice"),
-              (3, "Cocktail"),
-              (4, "Tea, Cocktail, Wine, Coffee, Water"),
-              (5, "Cocktail, Juice, ProteinShake, Soda"),
-              (6, "Tea, ProteinShake, Coffee, Soda"),
-              (7, "Water"),
-              (8, "Water, Cocoa, Wine"),
-              (9, "Juice, Cocoa, Water, Coffee, Tea, Cider"),
-              (10, "Cocoa, Tea, Water, Coffee, Wine"),
-              (11, "Soda, Tea, Coffee, ProteinShake, Juice"),
-              (12, "Cocoa, Cocktail, Juice, ProteinShake, Water"),
-              (13, "Coffee, Tea, ProteinShake, Cocktail, Water, Cider"),
-              (14, "Cocktail, Coffee, ProteinShake, Wine, Juice"),
-              (15, "Soda, Coffee, Cider"),
-              (16, "ProteinShake, Water, Coffee"),
-              (17, "ProteinShake, Cocktail, Ciderss"),
-              (18, "Coffee, Cocktail, Cocoa, Wine, ProteinShake, Water"),
-              (19, "Juice, Coffee, Soda"),
-              (20, "Soda")
-              ]
-
-            self.cursor.executemany(add_insert, add_values)
-            self.cnx.commit()
+            for table in self.tables:
+                add_insert = f"INSERT INTO {table} (TRANS_ID, ITEMS) VALUES (%s, %s)"
+                add_values = self.fetch_file_data(table)
+                self.cursor.executemany(add_insert, add_values)
+                self.cnx.commit()
         except mysql.Error as err:
             print(err)
             exit(1)
 
     def fetch_records(self):
-        items_list = list()
+        table_records = dict()
         try:
-            self.cursor.execute("SELECT ITEMS FROM DATASET_1")
-            for items in self.cursor:
-                for item in items:
-                    list_split = item.split(", ")
-                    items_list.append(set(list_split))
-            return items_list
+            for table in self.tables:
+                self.cursor.execute(f"SELECT ITEMS FROM {table}")
+                items_list = list()
+                for items in self.cursor:
+                    for item in items:
+                        list_split = item.split(", ")
+                        items_list.append(set(list_split))
+                table_records[table] = items_list
+            return table_records
         except mysql.Error as err:
             print(err)
             exit(1)
@@ -99,26 +141,10 @@ class Database:
 
 
 if __name__ == '__main__':
-    DB_NAME = 'cs634_data_mining_project'
 
-    config = {
-        'user': 'root',
-        'password': 'Maharaj3n$$$567',
-        'host': '127.0.0.1',
-        'raise_on_warnings': True
-    }
-
-    TABLES = dict()
-    TABLES['dataset_1'] = (
-        "CREATE TABLE `dataset_1` ("
-        "  `trans_id` int(11) NOT NULL AUTO_INCREMENT,"
-        "  `items` varchar(255) NOT NULL,"
-        "  PRIMARY KEY (`trans_id`)"
-        ") ENGINE=InnoDB")
-
-    db = Database(config)
-    db.connect_database(DB_NAME)
-    # db.create_table(TABLES)
-    # db.insert_records()
-    db.fetch_records()
+    db = Database()
+    db.connect_database()
+    db.create_table(TABLES)
+    db.insert_records()
+    print(db.fetch_records())
     db.close_connection()
